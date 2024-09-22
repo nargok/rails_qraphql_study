146f30cac3fcc8b1db614e97824e79a398a69e1e
nargok <nargok@gmail.com>
2024-09-22 19:34:38 +0900

    user books query with N+1

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -1,2 +1,12 @@
-User.create!(name: "Taro", email: "test1@example.com")
-User.create!(name: "Jiro", email: "test2@example.com")
+ActiveRecord::Base.transaction do
+
+  user1 = User.create!(name: "Taro", email: "test1@example.com")
+  user2 = User.create!(name: "Jiro", email: "test2@example.com")
+
+  user1.books.create!(title: "Ruby超入門")
+  user1.books.create!(title: "パーフェクトRails")
+  user1.books.create!(title: "Rubyレシピブック")
+
+end
+
+
